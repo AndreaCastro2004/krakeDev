413543c3339d005bbe92492a610046ee16e083b3
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-06 15:32:54 -0500

    El boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpimagenSaludo;
+    cmpimagenSaludo=document.getElementById("saludar");
+    cmpimagenSaludo.src="./imagenes/oke-yamada-ryo.gif"
     console.log("ingresa a saludar ");
 }
