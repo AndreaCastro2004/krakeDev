51a717459b81cca13b5860febbad009b5a49e265
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-07 11:21:18 -0500

    Mostrar lblDescuento

--- puntoDeVenta_v4/serviciosPuntoDeVenta.js
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -5,3 +5,9 @@ totalPagar=precio*cantidad
 return totalPagar;
 
 }
+
+calcularValorDescuento=function(monto,porcentajeDescuento){
+let valorDescuento;
+valorDescuento=((monto*porcentajeDescuento)/100);
+return valorDescuento;
+}
