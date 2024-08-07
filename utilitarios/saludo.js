d171c0d0a54ae838f747efc67276963ba52b8d01
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-07 09:23:02 -0500

    Separación de las funciones al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,10 +1,4 @@
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado
-}
+
 
 saludar=function(){
     //Recuperar el valor de la caja de texto txtNombre
@@ -21,34 +15,4 @@ saludar=function(){
     mostrarImagen("imgSaludo","./imagenes/imagen1.gif");
 
     mostrarTextoEnCaja("txtNombre","");
-}
-recuperarInt=function(idComponente){
-    let valorCaja= recuperarTexto(idComponente);
-    let valorEntero= parseInt(valorCaja);
-    return valorEntero;
-}
-
-RecuperarFloat=function(idComponente){
-    let valorCaja= recuperarTexto(idComponente);
-    let valorFloat= parseFloat(valorCaja);
-    return valorFloat;
-}
-mostrarTextoEnCaja=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.value=mensaje
-
-}
-
-mostrarTexto=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.innerText=mensaje
-}
-
-mostrarImagen=function(idComponente,rutaImagen){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.src=rutaImagen
-
 }
