d5e1e788edf8f1c160213685be4d3649b9bbc09e
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-10 21:55:39 -0500

    operadores logicos y de comparacion

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -9,22 +9,19 @@ calcularPromedioNotas=function(){
     valorDos=RecuperarFloat("txtNota2");
     valorTres=RecuperarFloat("txtNota3");
     promedio=calcularPromedio(valorUno,valorDos,valorTres);
-    
+    valorPromedio=promedio.toFixed(2);
 
     if(valorPromedio>0 && valorPromedio<5){
         mostrarTexto("lblMensaje","REPROBADO")
         mostrarImagen("lblImagen","./imagenes/imagen1.gif");
-        valorPromedio=promedio.toFixed(2);
         mostrarTexto("lblPromedio",valorPromedio);
     }else if(valorPromedio>=5 && valorPromedio<=8){
         mostrarTexto("lblMensaje","BUEN TRABAJO")
         mostrarImagen("lblImagen","./imagenes/imagen2.gif"); 
-        valorPromedio=promedio.toFixed(2);
         mostrarTexto("lblPromedio",valorPromedio);
     }else if(valorPromedio>8 && valorPromedio<=10){
         mostrarTexto("lblMensaje","EXELENTE")
         mostrarImagen("lblImagen","./imagenes/imagen3.gif"); 
-        valorPromedio=promedio.toFixed(2);
         mostrarTexto("lblPromedio",valorPromedio);
     }else{
         mostrarTexto("lblMensaje","DATOS INCORRECTOS")
