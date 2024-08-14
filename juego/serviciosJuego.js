2f5d0de039e4bec46112b5428a656eb13db9c398
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-14 10:59:28 -0500

    Generar Elemento, piedra papel o tijera

--- juego/serviciosJuego.js
+++ juego/serviciosJuego.js
@@ -4,8 +4,29 @@ obtenerAleatorio=function(){
     let entero;
     randon=Math.random();
     numero=randon*4
-    entero=parseInt(numero)
+    entero=parseInt(numero);
     if (entero>=1 && entero <=3){
         console.log(entero)
+        return entero;
+    }
+}
+generarElemento=function(){
+    let aleatorio;
+    let cadenaAleatorio;
+    aleatorio=obtenerAleatorio();
+    if(aleatorio==1){
+        cadenaAleatorio ="PIEDRA"
+        console.log(cadenaAleatorio)
+        return cadenaAleatorio;
+    }
+    if(aleatorio==2){
+        cadenaAleatorio="PAPEL"
+        console.log(cadenaAleatorio);
+        return cadenaAleatorio;
+    }
+    if(aleatorio==3){
+        cadenaAleatorio="TIJERA"
+        console.log(cadenaAleatorio);
+        return cadenaAleatorio;
     }
 }
