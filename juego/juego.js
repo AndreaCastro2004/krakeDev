906e2fbcf1ec436db7bc8fc61ce4f8dd68bfecba
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-14 14:47:01 -0500

    Se agrega nueva partida

--- juego/juego.js
+++ juego/juego.js
@@ -1,3 +1,5 @@
+puntosUsuario=0
+puntosComputador=0
 jugar=function(seleccionado){
     let elemento1;
     let computador
@@ -11,8 +13,26 @@ jugar=function(seleccionado){
     }
     if(ganador==1){
         mostrarTexto("lblGanador","PERDISTE LA PARTIDA")
+        puntosComputador=puntosComputador+1
+        mostrarTexto("lblPuntajeComputador",puntosComputador);
+        if(puntosComputador==5){
+            mostrarTexto("lblGanador","EL COMPUTADOR TE HA VENCIDO");
+        }
     }
     if(ganador==2){
         mostrarTexto("lblGanador","GANASTE LA PARTIDA")
+        puntosUsuario=puntosUsuario+1
+        mostrarTexto("lblJugador",puntosUsuario);
+        if(puntosUsuario==5){
+            mostrarTexto("lblGanador","HAS GANADO EL JUEGO")
+        }
     }
+}
+nuevaPartida=function(){
+    mostrarImagen("lblComputador", "");
+    mostrarTexto("lblGanador","");
+    mostrarTexto("lblPuntajeComputador","0.0");
+    mostrarTexto("lblJugador","0.0");
+    puntosUsuario=0
+    puntosComputador=0
 }
