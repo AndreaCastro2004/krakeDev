6e6d082fb660b75e3b640c28aeeb269c33a95869
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-14 12:14:05 -0500

    Determinar Ganador y generar Ruta

--- juego/serviciosJuego.js
+++ juego/serviciosJuego.js
@@ -15,18 +15,37 @@ generarElemento=function(){
     let cadenaAleatorio;
     aleatorio=obtenerAleatorio();
     if(aleatorio==1){
-        cadenaAleatorio ="PIEDRA"
-        console.log(cadenaAleatorio)
-        return cadenaAleatorio;
+        cadenaAleatorio +="PIEDRA"
     }
     if(aleatorio==2){
-        cadenaAleatorio="PAPEL"
-        console.log(cadenaAleatorio);
-        return cadenaAleatorio;
+        cadenaAleatorio +="PAPEL"
     }
     if(aleatorio==3){
-        cadenaAleatorio="TIJERA"
-        console.log(cadenaAleatorio);
-        return cadenaAleatorio;
+        cadenaAleatorio +="TIJERA"
     }
+    return cadenaAleatorio;
+ }
+
+determinarGanador=function(eleccionJugador1,eleccionJugador2){
+    let jugador;
+    if(eleccionJugador1 == eleccionJugador2){
+        jugador="0"
+        return jugador
+    }
+    if((eleccionJugador1 == "piedra" && eleccionJugador2 == "tijera") || (eleccionJugador1=="tijera" && 
+        eleccionJugador2=="papel")|| (eleccionJugador1=="papel" && eleccionJugador2== "piedra")){
+        jugador="1"
+        return jugador
+    }
+
+    if((eleccionJugador2 == "piedra" && eleccionJugador1 == "tijera") || (eleccionJugador2=="tijera" && 
+        eleccionJugador1=="papel")|| (eleccionJugador2=="papel" && eleccionJugador1== "piedra")){
+        jugador="2"
+        return jugador
+    }
+}
+generarRuta=function(nombre){
+    let ruta;
+    ruta="./imagenes/"+nombre+".png"
+    return ruta;
 }
