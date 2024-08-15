b017602e54a60161ad21a5b789e0630c174495af
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-15 11:31:32 -0500

    Completado PASO 0

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,1 +1,12 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula=function(caracter){
+    let letra;
+    let mayuscula=false
+    letra=caracter.charCodeAt(0);
+    if(letra>=65 && letra<=90){
+        mayuscula=true
+    }else{
+        mayuscula=false
+    }
+    return mayuscula;
+}
