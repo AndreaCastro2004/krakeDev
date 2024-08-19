3aac3d6b21bea5f24ef78563127ce2ebdca50e6f
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-19 11:26:39 -0500

    Funcion crearProdcuto

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -17,3 +17,20 @@ probarAtributos=function(){
         console.log("SI ESTA VIVO")
     }
 }
+crearProducto=function(){
+    let producto1={
+        nombre: "Deja",
+        precio:  1.55 ,
+        stock: 3
+    }
+    console.log(producto1.nombre);
+    let producto2={
+        nombre:"Papel industrial",
+        precio:1.50,
+        stock: 5
+    }
+    console.log(producto2.nombre);
+    if(producto1.stock<producto2.stock){
+        console.log("Producto 2 tiene mayor stock")
+    }
+}
