b4c3bd0a94e99b595eb496256ea8aa42d8ab64ad
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-08-20 10:31:19 -0500

    Evento Onload Agregado

--- roles/roles.js
+++ roles/roles.js
@@ -2,4 +2,18 @@ let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
 ]
-
+mostrarObcionEmpleado=function(){
+    mostrarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    ocultarComponente("divResumen");
+}
+mostrarObcionRol=function(){
+    mostrarComponente("divRol");
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divResumen");
+}
+mostrarObcionResumen=function(){
+    mostrarComponente("divResumen");
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divRol");
+}
